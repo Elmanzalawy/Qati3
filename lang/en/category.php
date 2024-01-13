09a0cd576e563cd75c2ab3e674db4ca7016542af
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2024-01-14 00:14:15 +0200

    add category model, migration, policy

---
+++ lang/en/category.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Category Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during category for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'category' => 'Category',
+    'categories' => 'Categories',
+    'subsidiaries' => 'Subsidiaries',
+    'subsidiary_of' => "Subsidiary of",
+    'established_on' => "Established on :date",
+
+    // FIELDS
+    'name' => 'Category name',
+    'name_en' => 'Category name (En)',
+    'name_ar' => 'Category name (Ar)',
+    'description' => 'Description',
+    'visible' => 'Visible',
+    'visibility' => 'Visibility',
+    'boycott_status' => 'Boycott status',
+    'parent_category' => 'Parent category',
+    'established_at' => 'Established at',
+
+    // ACTIONS
+    'add' => 'Add category',
+    'added' => 'Category added successfully.',
+    'updated' => 'Category updated successfully.',
+];
